b64e448f4775aa4cb02295a643ae15fcc60bb7e7
1-Inna-1 <151846574+1-Inna-1@users.noreply.github.com>
2024-02-09 22:23:08 +0700

    Update and rename Button_effects.py to button_effects.py

--- button_effects.py
+++ button_effects.py
@@ -1,4 +1,22 @@
 import pygame
+import os
+import sys
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('image', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
 class ImageButton:
     def __init__(self, x, y, width, height, text, image_path, image_cursor=None, sound_path=None):
         self.x = x
@@ -6,19 +24,22 @@ class ImageButton:
         self.width = width
         self.heignt = height
         self.text = text
-        self.image = pygame.image.load(image_path)
+        self.image = load_image(image_path)
         self.image = pygame.transform.scale(self.image, (width, height))
         self.cursor_image = self.image
         if image_cursor:
-            self.image_cursor = pygame.image.load(image_cursor)
+            self.image_cursor = load_image(image_cursor)
             self.image_cursor = pygame.transform.scale(self.image_cursor, (width, height))
         self.rect = self.image.get_rect(topleft=(x, y))
         self.sound = None
         if sound_path:
             self.sound = pygame.mixer.Sound(sound_path)
+        # наведена ли мышка на кнопку
         self.is_hovered = False
 
     def draw(self, screen):
+        #если мышка наведена на кнопку то отображаем картинку
+        #при наведении, если нет то отображаем другую картинку
         current_image = self.image_cursor if self.is_hovered else self.image
         screen.blit(current_image, self.rect.topleft)
 
@@ -26,9 +47,10 @@ class ImageButton:
         text_surface = font.render(self.text, True, (255, 255, 255))
         text_rect = text_surface.get_rect(center=self.rect.center)
         screen.blit(text_surface, text_rect)
-
+    #проверка наведена ли мышка
     def check_cursor(self, mouse_pos):
         self.is_hovered = self.rect.collidepoint(mouse_pos)
+
 
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and self.is_hovered:
